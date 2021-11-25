d3adeb7d7d9178622507d05b371e9b185340879d
James Morales <jamesmoralesmoreno@gmail.com>
2021-11-25 16:27:43 -0500

    feat(init): initial project

---
+++ Pet.Project.Store.Domain/Class1.cs
@@ -0,0 +1,10 @@
+﻿// <copyright file="Class1.cs" company="PetProject">
+// Copyright (c) PetProject. All rights reserved.
+// </copyright>
+
+namespace Pet.Project.Store.Domain
+{
+    public class Class1
+    {
+    }
+}
